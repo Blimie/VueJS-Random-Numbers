0fa186b06e0de26d3f529a4a8189b73d2f0cfca5
Blimie Kohn <blimiekohn@gmail.com>
2018-06-18 22:40:47 -0400

    Initial Commit

---
+++ 180618/Scripts/Index.js
@@ -0,0 +1,23 @@
+﻿new Vue({
+    el: '#app',
+    data: {
+        min: '',
+        max: '',
+        numbers: []
+    },
+    methods: {
+
+        randomNumClick: function () {
+            const randomNum = getRandomInt(+this.min, +this.max);
+            this.numbers.push(randomNum);
+        },
+
+        sortClick: function () {
+            this.numbers.sort((a, b) => a - b);
+        }
+    }
+});
+
+function getRandomInt(min, max) {     
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
